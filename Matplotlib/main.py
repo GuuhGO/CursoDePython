82e855c70e7440d917ab24493461f6ef6897e897
GuuhGO <gu.guimaraesrdc9@gmail.com>
2023-02-25 17:47:55 -0300

    -

--- Matplotlib/main.py
+++ Matplotlib/main.py
@@ -1,14 +1,6 @@
 import matplotlib.pyplot as plt
 from Ponto import *
+# plt.plot([0,10,5,15,3,20], marker='o')
 
-# plt.plot(p1.x,p1.y,'o')
-# plt.plot(p2.x,p2.y,'o')
 plt.plot([p1.x,p1.y],[p2.x,p2.y], marker='o')
 plt.show()
-
-""""
-plt.plot(coordendas1, coordenadas2)
-
- [1, 2]
- [2, 3]
-"""
